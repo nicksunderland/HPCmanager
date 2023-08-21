a1e68471e89049a1d4a3ea93cd1f2caced627769
nicksunderland <nicholas.p.sunderland@gmail.com>
2023-08-21 11:02:44 +0100

    back to using glue dependency

--- R/class_slurm.R
+++ R/class_slurm.R
@@ -13,11 +13,12 @@
 #' @slot max_time_mins integer
 #' @slot max_time_secs integer
 #' @slot mem_per_cpu integer.
-#' @slot script_path character.
-#' @slot run_path character.
+#' @slot r_version character.
+#' @slot rscript_options character.
 #'
 #' @return a Slurm object
 #' @importFrom methods slot new validObject callNextMethod
+#' @importFrom glue glue
 #' @export
 #'
 Slurm <- setClass(
@@ -34,8 +35,8 @@ Slurm <- setClass(
     max_time_mins = "integer",
     max_time_secs = "integer",
     mem_per_cpu = "integer",
-    script_path = "character",
-    run_path = "character"
+    r_version = "character",
+    rscript_options = "character"
 
     # https://slurm.schedmd.com/archive/slurm-16.05.8/sbatch.html
     # --dependency
@@ -61,92 +62,76 @@ Slurm <- setClass(
     max_time_hours = 1L,
     max_time_mins = 0L,
     max_time_secs = 0L,
-    script_path = character(),
-    run_path = character()
+    r_version = "4.2.1",
+    rscript_options = ""
   )
 )
 
 setMethod(
   f = "initialize",
   signature = "Slurm",
-  definition =function(.Object, job_name, account, script_path, ...) {
-    print("initialize")
+  definition =function(.Object, job_name, account, ...) {
     .Object <- callNextMethod(.Object, ...)
     .Object@job_name <- job_name
     .Object@account <- account
-    .Object@script_path <- get_script_path()
-    .Object@run_path <- sub(".R$", "_run.R", .Object@script_path)
 
-    print(.Object@run_path)
+    # Create the preamble
+    slurm_preamble <- glue("
+      #!/bin/bash
+      #SBATCH --job-name={.Object@job_name}
+      #SBATCH --partition={.Object@partition}
+      #SBATCH --cpus-per-task={as.character(.Object@cpu_per_task)}
+      #SBATCH --nodes={as.character(.Object@nodes)}
+      #SBATCH --cpus-per-task={as.character(.Object@cpu_per_task)}
+      #SBATCH --time={sprintf('%d-%02d:%02d:%02d',
+                              .Object@max_time_days,
+                              .Object@max_time_hours,
+                              .Object@max_time_mins,
+                              .Object@max_time_secs)}
+      #SBATCH --mem={as.character(.Object@mem_per_cpu)}M"
+    )
+    cat("Creating slurm header:\n", slurm_preamble, "\n")
 
-    writeLines(c("testing"), .Object@run_path)
+    # Get the path to 'this' R script and get its contents as a string
+    script_path <- get_script_path()
+    cat("Reading raw R script:\t", script_path, "\n")
+    raw_script_lines <- readLines(script_path)
+    raw_script <- paste0(raw_script_lines, collapse="\n")
 
-#
-#     # Create the preamble
-#     slurm_preamble <- paste0(
-#       c(
-#         "#!/bin/bash",
-#         paste0("#SBATCH --job-name=", .Object@job_name),
-#         paste0("#SBATCH --partition=", .Object@partition),
-#         paste0("#SBATCH --nodes=", as.character(.Object@nodes)),
-#         paste0("SBATCH --cpus-per-task=", as.character(.Object@cpu_per_task)),
-#         paste0("#SBATCH --time=", sprintf('%d-%02d:%02d:%02d',
-#                                           .Object@max_time_days,
-#                                           .Object@max_time_hours,
-#                                           .Object@max_time_mins,
-#                                           .Object@max_time_secs)),
-#         paste0("#SBATCH --mem=", as.character(.Object@mem_per_cpu), "M")
-#       ),
-#       collapse = "\n"
-#     )
-#     print(slurm_preamble)
-#
-#     # Create the R script
-#     r_fp <- .Object@script_path  #get_script_path()
-# cat("R script path: ", r_fp)
-#     script_dir <- dirname(r_fp)
-# cat("R script dir: ", script_dir)
-#     r_script <- readLines(r_fp)
-# cat("The r script")
-# print(r_script)
-#     r_script <- paste0(r_script, collapse="\n")
-#     run_script <- sub("library[(][\"']?HPCmanager[\"']?[)]", "", r_script)
-#     run_script <- sub("\n[0-9A-z_. ]*(?:<-|=)?[ ]*Slurm[(][A-z0-9= \"',.\n]*[)]", "", run_script)
-# cat("The run script")
-# print(run_script)
-#     run_script_fp <- sub(".R$", "_run.R", r_fp)
-# cat("The run script path: ", run_script_fp)
-#     writeLines(run_script, run_script_fp)
-#
-#
-#     # Create the bash script
-#     bash_lines <- paste0(
-#     c(
-#         slurm_preamble,
-#         "module load R",
-#         paste0("Rscript ", run_script_fp)
-#     ),
-#     collapse = "\n"
-#     )
-# cat("The bash script")
-# print(bash_lines)
-#
-#     bash_script_path <- sub("_run.R$", "_bash.sh", run_script_fp)
-# cat("The bash script path: ", bash_script_path)
-#     writeLines(bash_lines, bash_script_path)
-#
-#
-#     cat("Submitting bash script: ", bash_script_path)
-#     sbatch_return <- system(paste("sbatch", bash_script_path), intern = TRUE)
-#     job_id <- sub("Submitted batch job ", "", sbatch_return)
-#     cat("Job ID: ", job_id)
-#     print(sbatch_return)
-#
-#     opt <- options(show.error.messages = FALSE)
-#     on.exit(options(opt))
-#     suppressWarnings({stop()})
+    # create a new R script but remove the 'library(HPCmanager) and Slurm() object code
+    run_path <- sub(".R$", "_run.R", script_path)
+    run_script <- sub("library[(][\"']?HPCmanager[\"']?[)]", "", raw_script)
+    run_script <- sub("\n[-0-9A-z_. ]*(?:<-|=)?[ ]*Slurm[(][-A-z0-9= \"',.\n]*[)]", "", run_script)
+    cat("Creating new R script:\t", run_path, "\n")
+    writeLines(run_script, run_path)
 
-     .Object
+    # Create the bash script
+    bash_path <- sub(".R$", "_bash.sh", script_path)
+    bash_script <- glue(
+      "
+        {slurm_preamble}
+
+        module load languages/r/{.Object@r_version}
+
+        Rscript {.Object@rscript_options} {run_path}
+      "
+    )
+    cat("Creating bash script:\t", bash_path, "\n")
+    writeLines(bash_script, bash_path)
+
+    # Submit the bash script
+    cat("Submitting bash script.\n")
+    sbatch_return <- system(paste("sbatch", bash_path), intern = TRUE)
+    job_id <- sub("Submitted batch job ", "", sbatch_return)
+    cat("Job ID: ", job_id, "\n")
+    cat(sbatch_return, "\n")
+
+
+    opt <- options(show.error.messages = FALSE)
+    on.exit(options(opt))
+    suppressWarnings({stop()})
+
+     #.Object
 })
 
 
